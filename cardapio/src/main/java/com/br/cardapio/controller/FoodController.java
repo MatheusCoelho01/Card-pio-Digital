db6bb1305a0c24ea3da978e16ddafde56f9311c6
Matheus Coelho <matheus.cnasci@gmail.com>
2025-09-22 22:28:28 -0300

    adicionando conexão com banco postgresql

--- cardapio/src/main/java/com/br/cardapio/controller/FoodController.java
+++ cardapio/src/main/java/com/br/cardapio/controller/FoodController.java
@@ -1,7 +1,14 @@
 package com.br.cardapio.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/foods")
 public class FoodController {
+
+
+    public void getAll(){
+
+    }
 }
